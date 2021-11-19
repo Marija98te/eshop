b7e563ec8b650db85df71a0bc327d6f1ec3696bc
Marija <you@example.com>
2021-11-19 01:17:09 +0100

    End od section 3 API Architecture

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Infrastructure.Data;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
@@ -13,16 +14,16 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
-        public ProductsController(StoreContext context)  //This is called when is retrieved http request
+        private readonly IProductRepository _repo;
+        public ProductsController(IProductRepository repo)  //This is called when is retrieved http request
         {
-            _context = context;
+            _repo = repo;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()  //This is the response
         {
-            var products =await _context.Products.ToListAsync();
+            var products =await _repo.GetProductsAsync();
 
             return Ok(products);
         }
@@ -30,7 +31,19 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands() 
+        {
+            return Ok(await _repo.GetProductBrandsAsync());     
+        }
+
+        [HttpGet("genders")]
+        public async Task<ActionResult<IReadOnlyList<ProductGender>>> GetProductGenders() 
+        {
+            return Ok(await _repo.GetProductGendersAsync());     
         }
     }
 }
---
+++ Core/Entities/ProductGender.cs
@@ -0,0 +1,7 @@
+namespace Core.Entities
+{
+    public class ProductGender : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
