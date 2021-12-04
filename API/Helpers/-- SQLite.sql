f10226b95dab66495c8db0821621137f017d1fa3
Marija <you@example.com>
2021-12-04 16:23:13 +0100

    End of section 5

---
+++ API/Helpers/-- SQLite.sql
@@ -0,0 +1,37 @@
+-- SQLite
+INSERT INTO Products (Name, Description, Price, PictureUrl, ProductGenderId, ProductBrandId)
+VALUES ("Dolce & Gabbana/DG5027","Craftsmanship, refinement and timeless elegance: Dolce&Gabbana frames stand out for their unmistakable style, where the attention to details and the quality of materials are combined with Made in Italy creativity.",280.00,"images/products/dolce&gabbana_dg5027.png",1,1)
+,("Giorgio Armani/AR 309M","Elegant, modern and sophisticated, the Giorgio Armani collection stands out for the excellence of its manufacture, the extreme attention to detail, the purity of the lines and the use of high quality materials.",334.00,"images/products/giorgioarmani_ar309m.png",1,2)
+
+
+,("Armani Exchange/AX1017","A|X Armani Exchange is accessible Armani, inspired by street-chic culture and meant for the young generations or for those with a young mindset. Armani Exchange glasses frames are fast, affordable and metropolitan, without losing the active stance of the Giorgio Armani DNA.",123.00,"images/products/armaniexchange_ax1017.png",1,3)
+
+
+,("Prada/HERITAGE PR 16MV","Prada glasses are synonymous with cutting-edge style. All Prada frames reflect the brand's intellectual dimension, which combines structure and image through codes that go beyond trends.",285.00,"images/products/prada_heritagepr16mv.png",1,4)
+
+
+,("Ray-Ban/RB5169","Timeless style, authenticity and freedom of expression are the core values behind Ray-Ban glasses. The brand has been a leader in sun and prescription eyewear since its introduction of the iconic Aviator model, and today Ray-Ban eyeglasses and sunglasses remain at the forefront of cultural change - a symbol of self-expression.",149.00,"images/products/rayban_rb5169.png",1,5)
+
+
+,("Versace/VE3295","Made in Italy, Versace Eyewear provides the finishing touch to any look. Characterized by high-end materials and a recognizable design aesthetic, Versace eyeglasses and Versace sunglasses pair contemporary shapes with iconic brand codes.",264.00,"images/products/versace_ve3295.png",1,6)
+
+
+,("Dolce & Gabbana/DG5036","Craftsmanship, refinement and timeless elegance: Dolce&Gabbana frames stand out for their unmistakable style, where the attention to details and the quality of materials are combined with Made in Italy creativity.",307.00,"images/products/dolce&gabbana_dg5036.png",2,1)
+
+
+,("Giorgio Armani/AR7192","Elegant, modern and sophisticated, the Giorgio Armani collection stands out for the excellence of its manufacture, the extreme attention to detail, the purity of the lines and the use of high quality materials.",280.00,"images/products/giorgioarmani_ar7192.png",2,2)
+
+
+,("Armani Exchange/AX3053","A|X Armani Exchange is accessible Armani, inspired by street-chic culture and meant for the young generations or for those with a young mindset. Armani Exchange glasses frames are fast, affordable and metropolitan, without losing the active stance of the Giorgio Armani DNA.",117.00,"images/products/armaniexchange_ax3035.png",2,3)
+
+
+,("Prada/PR 04WV","Prada glasses are synonymous with cutting-edge style. All Prada frames reflect the brand's intellectual dimension, which combines structure and image through codes that go beyond trends.",288.00,"images/products/prada_pr04wv.png",2,4)
+
+
+,("Ray-Ban/RX5387","Timeless style, authenticity and freedom of expression are the core values behind Ray-Ban glasses. The brand has been a leader in sun and prescription eyewear since its introduction of the iconic Aviator model, and today Ray-Ban eyeglasses and sunglasses remain at the forefront of cultural change - a symbol of self-expression.",153.00,"images/products/rayban_rx5387.png",2,5)
+
+
+,("Versace/VE1271","Made in Italy, Versace Eyewear provides the finishing touch to any look. Characterized by high-end materials and a recognizable design aesthetic, Versace eyeglasses and Versace sunglasses pair contemporary shapes with iconic brand codes.",251.00,"images/products/versace_ve1271.png",2,6);
+
+DROP TABLE Products
+SELECT * FROM Products
